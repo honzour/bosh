0b88c040422bf595ee1bfb032749a4034772fdbb
honza <honza@debian.cz>
2017-04-26 22:21:33 +0200

    removed appcompat balast + added gps listener

--- android/ImageUpload/app/src/main/java/cz/bosh/imageupload/MainActivity.java
+++ android/ImageUpload/app/src/main/java/cz/bosh/imageupload/MainActivity.java
@@ -1,13 +1,78 @@
 package cz.bosh.imageupload;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private LocationManager mlocManager;
+    private LocationListener mlocListener;
+    private TextView mLongitude;
+    private TextView mLatitude;
+    private TextView mAccuracy;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mLongitude = (TextView) findViewById(R.id.activity_main_longitude);
+        mLatitude = (TextView) findViewById(R.id.activity_main_latitude);
+        mAccuracy = (TextView) findViewById(R.id.activity_main_accuracy);
+
+        mlocManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        mlocListener = new LocationListener()
+        {
+            @Override
+            public void onLocationChanged(Location location) {
+                mLongitude.setText(String.valueOf(location.getLongitude()));
+                mLatitude.setText(String.valueOf(location.getLatitude()));
+                mAccuracy.setText(String.valueOf(location.getAccuracy()));
+
+
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Toast.makeText(MainActivity.this, R.string.no_gps, Toast.LENGTH_LONG).show();
+                mlocManager.removeUpdates(mlocListener);
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+        };
+    }
+
+    @Override
+    protected void onPause() {
+        if (mlocListener != null && mlocManager != null)
+            mlocManager.removeUpdates(mlocListener);
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!mlocManager.isProviderEnabled(LocationManager.GPS_PROVIDER))
+        {
+            Toast.makeText(MainActivity.this, R.string.no_gps, Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            mlocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mlocListener);
+        }
     }
 }
