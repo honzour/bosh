21e74b4096c1dc3cfc32a8a394f242a71a900263
honza <honza@debian.cz>
2017-05-02 03:00:17 +0200

    add HTML form

---
+++ web/add.php
@@ -0,0 +1,15 @@
+<HTML>
+<HEAD><meta charset="utf-8"></HEAD>
+<BODY>
+<FORM action="add.php" method="post" enctype="multipart/form-data">
+<TABLE>
+<TR><TD>Délka:</TD><TD><INPUT type="text" name="lon"></TD></TR>
+<TR><TD>Šířka:</TD><TD><INPUT type="text" name="lat"></TD></TR>
+<TR><TD>Přesnost:</TD><TD><INPUT type="text" name="acc"></TD></TR>
+<TR><TD>Fotka:</TD><TD><INPUT type = "file" name="file"></TD></TR>
+</TABLE>
+<P>
+<INPUT type = "submit">
+</FORM>
+</BODY>
+</HTML>
