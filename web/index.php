c12261affc5488fa6db13ff91e389b28972d5e83
honza <honza@debian.cz>
2017-05-02 02:51:10 +0200

    fix errors, delete

--- web/index.php
+++ web/index.php
@@ -8,10 +8,17 @@
 $db = mysql_connect("localhost", "root", "korinek");
 mysql_select_db("bosh",$db); 
 
+if ($_GET["action"] == "delete") {
+	$id = mysql_escape_string($_GET["id"]);
+
+	echo "DELETE FROM photos WHERE id = '" . $id . "'";
+	mysql_query("DELETE FROM photos WHERE id = '" . $id . "'", $db);
+}
+
 $result = mysql_query("SELECT * FROM photos",$db);
 
 while ($row = mysql_fetch_row($result)) {
-	echo("<TD>" . $row[0] . "</TD><TD>" . $row[1] . "</TD><TD>" . $row[2] . "</TD><TD>" . $row[3] . "</TD><TD><A href=\"delete.php?id=" . $row[0] ."\">smazat</A></TD>");
+	echo("<TR><TD>" . $row[0] . "</TD><TD>" . $row[1] . "</TD><TD>" . $row[2] . "</TD><TD>" . $row[3] . "</TD><TD><A href=\"index.php?action=delete&amp;id=" . $row[0] ."\">smazat</A></TD></TR>");
 }
 mysql_free_result($result);
 mysql_close($db)
