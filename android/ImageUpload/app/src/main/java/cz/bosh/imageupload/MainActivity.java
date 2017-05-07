f70f06beb2d1e05b1ea7a55c5417cb3ad87fc51b
honza <honza@debian.cz>
2017-05-07 03:41:03 +0200

    file upload works

--- android/ImageUpload/app/src/main/java/cz/bosh/imageupload/MainActivity.java
+++ android/ImageUpload/app/src/main/java/cz/bosh/imageupload/MainActivity.java
@@ -16,9 +16,15 @@ import android.widget.Toast;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -107,9 +113,70 @@ public class MainActivity extends Activity {
         }
     }
 
+    private static void doPostWithImage(String url, Map<String, String> args, String pathToImage) {
+
+        try {
+
+            File binaryFile = new File(pathToImage);
+            String boundary = Long.toHexString(System.currentTimeMillis()); // Just generate some unique random value.
+            String CRLF = "\r\n"; // Line separator required by multipart/form-data.
+
+            URLConnection connection = new URL(url).openConnection();
+            connection.setDoOutput(true);
+            connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+
+            final String charset = "UTF-8";
+
+            OutputStream output = connection.getOutputStream();
+            PrintWriter writer = new PrintWriter(new OutputStreamWriter(output, charset), true);
+
+
+            // Send normal params
+            for (Map.Entry<String, String> entry : args.entrySet()) {
+                writer.append("--" + boundary).append(CRLF);
+                writer.append("Content-Disposition: form-data; name=\"" + entry.getKey() + "\"").append(CRLF);
+                writer.append("Content-Type: text/plain; charset=" + charset).append(CRLF);
+                writer.append(CRLF).append(entry.getValue()).append(CRLF).flush();
+            }
+
+            // Send binary file.
+            writer.append("--" + boundary).append(CRLF);
+            writer.append("Content-Disposition: form-data; name=\"file\"; filename=\"" + binaryFile.getName() + "\"").append(CRLF);
+            writer.append("Content-Type: " + URLConnection.guessContentTypeFromName(binaryFile.getName())).append(CRLF);
+            writer.append("Content-Transfer-Encoding: binary").append(CRLF);
+            writer.append(CRLF).flush();
+
+
+            InputStream input = new FileInputStream(binaryFile);
+
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = input.read(buffer)) != -1)
+            {
+                output.write(buffer, 0, bytesRead);
+            }
+            input.close();
+
+
+            output.flush(); // Important before continuing with writer!
+            writer.append(CRLF).flush(); // CRLF is important! It indicates end of boundary.
+
+            // End of multipart/form-data.
+            writer.append("--" + boundary + "--").append(CRLF).flush();
+
+            int responseCode = ((HttpURLConnection) connection).getResponseCode();
+            Toast.makeText(ImageApplication.imageApplication, "Server response: " + String.valueOf(responseCode), Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            Toast.makeText(ImageApplication.imageApplication, e.toString(), Toast.LENGTH_LONG).show();Toast.makeText(ImageApplication.imageApplication, e.toString(), Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+
     private static void doPost(String url, Map<String, String> args) {
 
         try {
+            Toast.makeText(ImageApplication.imageApplication, "Sending GPS coordinates..." , Toast.LENGTH_LONG).show();
             URL obj = new URL(url);
             HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
@@ -150,10 +217,10 @@ public class MainActivity extends Activity {
             }
             in.close();
 
-            Toast.makeText(ImageApplication.imageApplication, String.valueOf(responseCode), Toast.LENGTH_LONG).show();
+            Toast.makeText(ImageApplication.imageApplication, "Server response: " + String.valueOf(responseCode), Toast.LENGTH_LONG).show();
         }
         catch (Exception e) {
-            System.out.println(e);
+            Toast.makeText(ImageApplication.imageApplication, e.toString(), Toast.LENGTH_LONG).show();
         }
     }
 
@@ -162,7 +229,10 @@ public class MainActivity extends Activity {
         map.put("lon", String.valueOf(lon));
         map.put("lat", String.valueOf(lat));
         map.put("acc", String.valueOf(acc));
-        doPost("http://backpropagation.wz.cz/bosh/add.php", map);
+      //  doPost("http://backpropagation.wz.cz/bosh/add.php", map);
+
+        
+        doPostWithImage("http://backpropagation.wz.cz/bosh/add.php", map, "/storage/sdcard1/DCIM/Camera/i.jpg");
     }
 
     @Override
