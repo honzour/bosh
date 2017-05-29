bda287e1a06a5c8dd4e3ca1228fa64069fabc5e6
honza <honza@debian.cz>
2017-05-29 21:32:18 +0200

    note in utf8

--- web/index.php
+++ web/index.php
@@ -5,13 +5,11 @@ include("utils.php");
 htmlHeader("Seznam obrázků");
 
 ?>
-<TABLE border="3">
-<TR><TD>id</TD><TD>délka</TD><TD>šířka</TD><TD>přesnost</TD><TD>funkce</TD></TR>
-<TR><TD colspan="5">obrázek</TD></TR>
 <?php
 	
 
 	$db = mysql_connect($db_host, $db_user, $db_password);
+    mysql_set_charset ("utf8", $db);
 	mysql_select_db($db_db, $db);
 
 	if (array_key_exists("action", $_GET) && $_GET["action"] == "delete") {
@@ -20,16 +18,27 @@ htmlHeader("Seznam obrázků");
 		mysql_query("DELETE FROM photos WHERE id = '" . $id . "'", $db);
 	}
 
-	$result = mysql_query("SELECT id, lon, lat, acc, photo FROM photos",$db);
+	$result = mysql_query("SELECT id, lon, lat, acc, photo, note FROM photos",$db);
 
 	while ($row = mysql_fetch_row($result)) {
-		echo("<TR><TD>" . $row[0] . "</TD><TD>" . $row[1] . "</TD><TD>" . $row[2] . "</TD><TD>" . $row[3] . "</TD><TD><A href=\"index.php?action=delete&amp;id=" . $row[0] ."\">smazat</A></TD></TR>\n");
-		echo("<TR><TD colspan=\"5\"><IMG src=\"data:image/png;base64,". base64_encode($row[4]) ."\" alt=\"fotka\"></TD></TR>\n");
+?>
+<P>
+<?php
+		echo("<H3> obrázek " . $row[0] . "</H3>\n");
+        echo("délka: " . $row[1] . "<BR>\n");
+        echo("šířka: " . $row[2] . "<BR>\n");
+        echo("přesnost: " . $row[3] . "<BR><BR>\n");
+        echo("poznámka: " . htmlspecialchars($row[5]) . "<BR><BR>\n");
+		echo("<IMG src=\"data:image/png;base64,". base64_encode($row[4]) ."\" alt=\"fotka\"><BR>\n");
+		echo("<A href=\"index.php?action=delete&amp;id=" . $row[0] ."\">smazat</A>\n<BR><BR>");
+?>
+</P>
+<HR>
+<?php
 	}
 	mysql_free_result($result);
 	mysql_close($db)
 
  ?>
-</TABLE>
 </BODY>
 </HTML>
