adce66ca32546ac251418898db2de86ed4d18dfc
honza <honza@debian.cz>
2017-08-25 07:16:40 +0200

    inserting

--- web/admin.php
+++ web/admin.php
@@ -20,94 +20,154 @@ Nahrávám csv soubor...<BR>
 	}
 	else
 	{
-			$csvFile = file_get_contents($tmpName);
-			if (!$csvFile)
+		$csvFile = file_get_contents($tmpName);
+		if (!$csvFile)
+		{
+?>
+		Soubor se nepodařilo otevřít<BR>
+<?php
+		}
+		else
+		{
+?>
+		Parsuji soubor...<BR>
+<?php
+			$rows = explode("\n", $csvFile);
+			if (!$rows)
 			{
-			?>
-				Soubor se nepodařilo otevřít<BR>
-			<?php
-
+?>
+				Soubor se nepodařilo rozdělit na řádky<BR>
+<?php
 			}
 			else
 			{
-				?>
-					Parsuji soubor...<BR>
-				<?php
-				$rows = explode("\n", $csvFile);
-				if (!$rows)
+				$db = mysql_connect($db_host, $db_user, $db_password);
+   				mysql_set_charset ("utf8", $db);
+				mysql_select_db($db_db, $db);
+				mysql_query("delete from shops");
+				mysql_query("delete from brands");
+				$ok = true;
+?>
+				Soubor má <?php echo(count($rows) - 1); ?> řádků<BR>
+<?php
+				$i = 0;
+				foreach($rows as $row => $data)
 				{
-				?>
-					Soubor se nepodařilo rozdělit na řádky<BR>
-				<?php
+					$i++;
+					if (count($rows) <= $row + 1)
+						break;
+					$fields = explode(",", $data);
+					if (count($fields) != 10) {
+						echo("Chyba na řádku " . ($row + 1) . ":<BR>" . htmlspecialchars($data));
+						echo("<BR>Musí jít o 10 csv oddělených údajů, ale je jich tam " . count($fields));
+						$ok = false;
+						break;
+					} else
+					{
+//							1, Jan Čarný, 2, Karel Otrok, COOP, 1, Kozolupy, Bubovická 7, 14.25, 50.325
+						$person1_id = mysql_escape_string($fields[0]);
+						$person1 = mysql_escape_string($fields[1]);
+						$person2_id = mysql_escape_string($fields[2]);
+						$person2 = mysql_escape_string($fields[3]);
+						$brand = mysql_escape_string($fields[4]);
+						$shop_id = mysql_escape_string($fields[5]);
+						$city = mysql_escape_string($fields[6]);
+						$street = mysql_escape_string($fields[7]);
+						$lon = mysql_escape_string($fields[8]);
+						$lat = mysql_escape_string($fields[9]);
+
+						$res = mysql_query("select count(1) as cnt from people where id = $person1_id and kam = 1");
+						$cnt = mysql_fetch_row($res)[0];
+						if (!$cnt) {
+							echo("V databázi není žádný KAM $person1 s id $person1_id, chyba na řádku $i");
+							$ok = false;
+							break;
+						}
+						$res = mysql_query("select count(1) as cnt from people where id = $person2_id and oz = 1");
+						$cnt = mysql_fetch_row($res)[0];
+						if (!$cnt) {
+							echo("V databázi není žádný OZ $person2 s id $person2_id, chyba na řádku $i");
+							$ok = false;
+							break;
+						}
+					}
 				}
-				else
-				{
-
-					$db = mysql_connect($db_host, $db_user, $db_password);
-    				mysql_set_charset ("utf8", $db);
-					mysql_select_db($db_db, $db);
-					mysql_query("delete from shops");
-					mysql_query("delete from brands");
-
-					$ok = true;
-				?>
-					Soubor má <?php echo(count($rows) - 1); ?> řádků<BR>
-					<?php
+				if ($ok) {
+					echo("Kontrola dat: v pořádku<BR>\n");
+					echo("Nahrávám obchodní řetězce...<BR>\n");
+					$i = 0;
 					foreach($rows as $row => $data)
 					{
+						$i++;
 						if (count($rows) <= $row + 1)
 							break;
 						$fields = explode(",", $data);
-						if (count($fields) != 10) {
-							echo("Chyba na řádku " . ($row + 1) . ":<BR>" . htmlspecialchars($data));
-							echo("<BR>Musí jít o 10 csv oddělených údajů, ale je jich tam " . count($fields));
+						$brand = mysql_escape_string($fields[4]);
+						$res = mysql_query("select count(1) as cnt from brands where name = '$brand'");
+						if (!$res) {
 							$ok = false;
+							echo("Chyba na řádku $i: ");
+							echo(mysql_error());
 							break;
-						} else
-						{
-//							1, Jan Čarný, 2, Karel Otrok, COOP, 1, Kozolupy, Bubovická 7, 14.25, 50.325
-							$person1_id = mysql_escape_string($fields[0]);
-							$person1 = mysql_escape_string($fields[1]);
-							$person2_id = mysql_escape_string($fields[2]);
-							$person2 = mysql_escape_string($fields[3]);
-							$brand = mysql_escape_string($fields[4]);
-							$shop_id = mysql_escape_string($fields[5]);
-							$city = mysql_escape_string($fields[6]);
-							$street = mysql_escape_string($fields[7]);
-							$lon = mysql_escape_string($fields[8]);
-							$lat = mysql_escape_string($fields[9]);
-
-							$res = mysql_query("select count(1) as cnt from people where id = $person1_id and kam = 1");
-							$cnt = mysql_fetch_row($res)[0];
-							if (!$cnt) {
-								echo("V databázi není žádný KAM $person1 s id $person1_id");
-								$ok = false;
-								break;
-							}
-							$res = mysql_query("select count(1) as cnt from people where id = $person2_id and oz = 1");
-							$cnt = mysql_fetch_row($res)[0];
-							if (!$cnt) {
-								echo("V databázi není žádný OZ $person2 s id $person2_id");
-								$ok = false;
-								break;
-							}
 						}
+						$cnt = mysql_fetch_row($res)[0];
+						if ($cnt < 1) {
+							$res = mysql_query("insert into brands (name) values ('$brand')");
+							if (!$res) {
+								$ok = false;
+								echo("Chyba na řádku $i: ");
+								echo(mysql_error());
+								break;
+							}
+						} // if ($cnt < 1)
+					} // foreach
+				} // if ($ok)
+				if ($ok) {
+					echo("Řetězce nahrány<BR>\n");
+					echo("Nahrávám obchody....<BR>\n");
+					$i = 0;
+					foreach($rows as $row => $data)
+					{
+						$i++;
+						if (count($rows) <= $row + 1)
+							break;
+						$fields = explode(",", $data);
+						$person1_id = mysql_escape_string($fields[0]);
+						$person1 = mysql_escape_string($fields[1]);
+						$person2_id = mysql_escape_string($fields[2]);
+						$person2 = mysql_escape_string($fields[3]);
+						$brand = mysql_escape_string($fields[4]);
+						$shop_id = mysql_escape_string($fields[5]);
+						$city = mysql_escape_string($fields[6]);
+						$street = mysql_escape_string($fields[7]);
+						$lon = mysql_escape_string($fields[8]);
+						$lat = mysql_escape_string($fields[9]);
 
+						$res = mysql_query("insert into shops(id, kam, oz, brand, city, street, lon, lat) values (" .
+							"'$shop_id', " .
+							"'$person_1_id', " .
+							"'$person_2_id', " .
+						")");
+						if (!$res) {
+							$ok = false;
+							echo("Chyba na řádku $i: ");
+							echo(mysql_error());
+							break;
+						}
 					}
 				}
-
-			}
-
+			} // else
+		}
 	}
 ?>
 </P>
 <P>
 <A HREF="index.php">Zpět na hlavní stránku</A><BR>
-<A HREF="people.php">Osoby</A><BR>
+<A HREF="people.php">Editace osob</A><BR>
 <A HREF="admin.php">Zpět na administraci</A>
 </P>
 	<?php
-}
+} // if (array_key_exists("csv", $_POST))
 else
 {
 	?>
@@ -122,10 +182,10 @@ else
 </FORM>
 <P>
 <A HREF="index.php">Zpět na hlavní stránku</A><BR>
-<A HREF="people.php">Osoby</A><BR>
+<A HREF="people.php">Editace osob</A><BR>
 </P>
 
-	<?php
+<?php
 }
 ?>
 
