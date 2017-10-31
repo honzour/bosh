ffbb9f480aedca4be2297fb7e9c7ae1f9c3678f1
Honza <prase@prase.cz>
2017-10-31 23:32:50 +0100

    select

---
+++ android/ImageUpload/app/src/main/java/cz/bosh/imageupload/RecordsActivity.java
@@ -0,0 +1,38 @@
+package cz.bosh.imageupload;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.List;
+
+/**
+ * Created by honza on 31.10.17.
+ */
+
+public class RecordsActivity extends Activity {
+
+    ListView mList;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final List<Long> ids = ImageApplication.database.selectAll();
+
+        setContentView(R.layout.records);
+        mList = (ListView) findViewById(R.id.records_list);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.records_item, R.id.records_item_text, ids);
+        mList.setAdapter(adapter);
+        mList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Database.Record dr = ImageApplication.database.selectById(ids.get(i));
+                System.out.println(dr);
+            }
+        });
+    }
+}
