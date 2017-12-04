080d21be3aa838ed0e4ad6fbb7120cc8bbe2a785
Honza <prase@prase.cz>
2017-12-04 21:05:55 +0100

    migrace

--- sql/migrace.sql
+++ sql/migrace.sql
@@ -1,4 +1,17 @@
 alter table shops add nid int;
 
---update shops set id = nid;
+-- provest import
+
+update photos p set p.nid = (select s.nid from shops s where s.id = p.id);
+
+alter table photos add column nid int;
+update photos set shop = nid;
+alter table photos drop column nid;
+alter table shops drop column id;
+alter table shops add column id int;
+update shops set id = nid;
+
+alter table shops drop column nid;
+
+
 
