862a12b198f6def8d01524fbc2d09184c020d363
Honza <prase@prase.cz>
2018-02-14 01:33:14 +0100

    + export import channel

--- web/admin.php
+++ web/admin.php
@@ -63,9 +63,9 @@ Nahrávám csv soubor...<BR>
 					if (count($rows) <= $row + 1)
 						break;
 					$fields = explode("$", $data);
-					if (count($fields) != 10) {
+					if (count($fields) != 11) {
 						echo("Chyba na řádku " . ($row + 1) . ":<BR>" . htmlspecialchars($data));
-						echo("<BR>Musí jít o 10 csv oddělených údajů, ale je jich tam " . count($fields));
+						echo("<BR>Musí jít o 11 csv oddělených údajů, ale je jich tam " . count($fields));
 						$ok = false;
 						break;
 					} else
@@ -148,6 +148,7 @@ Nahrávám csv soubor...<BR>
 						$street = mysql_escape_string(trim($fields[7]));
 						$lon = mysql_escape_string(trim($fields[8]));
 						$lat = mysql_escape_string(trim($fields[9]));
+						$channel = mysql_escape_string(trim($fields[10]));
 						$query = "select id from brands where name = '$brand'";
 						$res = mysql_query($query);
 						if (!$res) {
@@ -158,7 +159,7 @@ Nahrávám csv soubor...<BR>
 						}
 						$brand_id = mysql_fetch_row($res)[0];
 
-						$query = "insert into shops(id, boss, worker, brand, city, street, lon, lat) values (" .
+						$query = "insert into shops(id, boss, worker, brand, city, street, lon, lat, channel) values (" .
 							"'$shop_id', " .
 							"$person1_id, " .
 							"$person2_id, " .
@@ -166,7 +167,8 @@ Nahrávám csv soubor...<BR>
 							"'$city', " .
 							"'$street', " .
 							"$lon, " .
-							"$lat" .
+							"$lat," .
+							"'$channel'" .
 						")";
 
 						$res = mysql_query($query);
--- web/csv.php
+++ web/csv.php
@@ -16,7 +16,7 @@ include("utils.php");
 		$query = "select concat_ws('$', b.name, s.street, s.city, s.id, s.lon, s.lat) from shops s left join brands b on s.brand = b.id left join people p on p.id = s.worker where p.id='" .
 			$person_id . "' order by s.id";
 	} else {
-		$query = "select concat_ws('$ ', s.boss, k.name, s.worker, o.name, b.name, s.id, s.city, s.street, s.lon, s.lat) from shops s left join brands b on b.id = s.brand left join people k on k.id = s.boss left join people o on o.id = s.worker order by s.id";
+		$query = "select concat_ws('$ ', s.boss, k.name, s.worker, o.name, b.name, s.id, s.city, s.street, s.lon, s.lat, s.channel) from shops s left join brands b on b.id = s.brand left join people k on k.id = s.boss left join people o on o.id = s.worker order by s.id";
 	}
 
 	$res = mysql_query($query);
